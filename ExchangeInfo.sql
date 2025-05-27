2d52394c5114530e2c7f6104006465f8eaf23f5d
Miral Tripathi <mtripath@uchicago.edu>
2025-05-26 22:19:12 -0500

    modified:   ExchangeInfo.sql added regulator data connected regulator data to exchange info

--- ExchangeInfo.sql
+++ ExchangeInfo.sql
@@ -32,6 +32,60 @@ CREATE TABLE ExchangeInfo.Is_Observed_By (
     Exchange_Id INT
 );
 
+-- Table: Regulators
+CREATE TABLE ExchangeInfo.Regulators (
+    Regulator_ID INT PRIMARY KEY,
+    Regulator_Name VARCHAR(255),
+    Regulator_Type VARCHAR(100),
+    Address VARCHAR(255),
+    Contact_Information_Website VARCHAR(255),
+    Contact_Information_Phone_Number VARCHAR(20)
+);
+
+-- Insert data into Regulators
+INSERT INTO ExchangeInfo.Regulators VALUES
+(1, 'U.S. Securities and Exchange Commission (SEC)', 'Federal Agency', '100 F Street NE, Washington, DC 20549', 'https://www.sec.gov', '202-551-6551'),
+(2, 'Commodity Futures Trading Commission (CFTC)', 'Federal Agency', '1155 21st Street NW, Washington, DC 20581', 'https://www.cftc.gov', '202-418-5000'),
+(3, 'Financial Industry Regulatory Authority (FINRA)', 'Self-Regulatory Organization', '1735 K Street NW, Washington, DC 20006', 'https://www.finra.org', '301-590-6500'),
+(4, 'National Futures Association (NFA)', 'Self-Regulatory Organization', '300 S. Riverside Plaza, Suite 1800, Chicago, IL 60606', 'https://www.nfa.futures.org', '312-781-1300'),
+(5, 'Municipal Securities Rulemaking Board (MSRB)', 'Self-Regulatory Organization', '1300 I Street NW, Suite 1000, Washington, DC 20005', 'https://www.msrb.org', '202-838-1500');
+
+-- Table: Is_Supervised_By with COMPOSITE PRIMARY KEY
+CREATE TABLE ExchangeInfo.Is_Supervised_By (
+    Exchange_ID INT,
+    Regulator_ID INT,
+    PRIMARY KEY (Exchange_ID, Regulator_ID),
+    FOREIGN KEY (Exchange_ID) REFERENCES Exchanges(Exchange_ID),
+    FOREIGN KEY (Regulator_ID) REFERENCES Regulators(Regulator_ID)
+);
+
+-- Insert relationships
+INSERT INTO ExchangeInfo.Is_Supervised_By VALUES
+(0, 1),  -- NYSE - SEC
+(0, 3),  -- NYSE - FINRA
+(1, 1),  -- NASDAQ - SEC
+(1, 3),  -- NASDAQ - FINRA
+(2, 1),  -- CBOE - SEC
+(2, 2),  -- CBOE - CFTC
+(2, 3),  -- CBOE - FINRA
+(3, 1),  -- CBOE BZX - SEC
+(3, 3),  -- CBOE BZX - FINRA
+(4, 1),  -- CBOE BYX - SEC
+(4, 3),  -- CBOE BYX - FINRA
+(5, 1),  -- CBOE EDGA - SEC
+(5, 3),  -- CBOE EDGA - FINRA
+(6, 1),  -- CBOE EDGX - SEC
+(6, 3),  -- CBOE EDGX - FINRA
+(7, 1),  -- CBOE C2 - SEC
+(7, 3),  -- CBOE C2 - FINRA
+(8, 1),  -- IEX - SEC
+(8, 3),  -- IEX - FINRA
+(9, 1),  -- MIAX - SEC
+(9, 3),  -- MIAX - FINRA
+(10, 2), -- CME - CFTC
+(10, 4); -- CME - NFA
+
+
 INSERT INTO ExchangeInfo.Holiday(Holiday_Id, Holiday_Name, Is_Early_Close, Holiday_Date, Calendar_Year)
 VALUES
 	(1, "New Year's Day", False, "2025-01-01", 2025),
