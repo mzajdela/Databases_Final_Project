3dd03e263b04370e2373637ecfa66180fffa7554
Casey Cummo <ccummo@uchicago.edu>
2025-05-26 20:35:55 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -120,31 +120,31 @@ if __name__ == "__main__":
     top_frame.pack(fill=tk.X)
     query_1 = tk.Label(top_frame, text=query_list_text, wraplength=400, justify="left")
     query_1.grid(row=0, column=0, columnspan=2, sticky="w", pady=(0, 5))
-    instruction_label = tk.Label(top_frame, text="Choose a number to run the corresponding query:", justify="left")
+    instruction_label = tk.Label(top_frame, text="Enter a number to run the corresponding query:", justify="left")
     instruction_label.grid(row=1, column=0, sticky="w", pady=5)
     query_input = tk.Entry(top_frame, width=5)
     query_input.grid(row=1, column=1, sticky="w", padx=5, pady=5)
     # Set the submit_button command to this commented line for result
     # command=lambda: run_query(int(entry.get()) - 1)
-    run_query_button = tk.Button(top_frame, text="Run Query")
+    run_query_button = tk.Button(top_frame, text="Submit")
     run_query_button.grid(row=1, column=2, sticky="w", padx=5, pady=5)
 
     # Middle Frame - Insert Statements
     # Insert Asset Class
     middle_frame = tk.Frame(root, padx=10, pady=10)
     middle_frame.pack(fill=tk.X)
-    asset_class_label = tk.Label(middle_frame, text="Asset Class:")
+    asset_class_label = tk.Label(middle_frame, text="Insert new Asset Class (AssetClassName):")
     asset_class_label.grid(row=0, column=0, sticky="e", padx=5, pady=5)
     asset_class_text_box = tk.Entry(middle_frame)
     asset_class_text_box.grid(row=0, column=1, sticky="w", padx=5, pady=5)
-    insert_button = tk.Button(middle_frame, text="Insert Asset")
+    insert_button = tk.Button(middle_frame, text="Submit")
     insert_button.grid(row=0, column=2, padx=5, pady=5)
     # Insert Exchange
-    exchange_label = tk.Label(middle_frame, text="Exchange:")
+    exchange_label = tk.Label(middle_frame, text="Insert new Exchange Info (ExchangeName,Address,TimeZone,HasPhysicalTradingFloor):")
     exchange_label.grid(row=1, column=0, sticky="e", padx=5, pady=5)
     exchange_text_box = tk.Entry(middle_frame)
     exchange_text_box.grid(row=1, column=1, sticky="w", padx=5, pady=5)
-    insert_exchange_button = tk.Button(middle_frame, text="Insert Exchange")
+    insert_exchange_button = tk.Button(middle_frame, text="Submit")
     insert_exchange_button.grid(row=1, column=2, padx=5, pady=5)
 
     # Bottom Frame - Display Results
@@ -153,7 +153,7 @@ if __name__ == "__main__":
     result_label = tk.Label(bottom_frame, text="", anchor="w", width=60)
     result_label.grid(row=0, column=0, sticky="w")
     exit_button = tk.Button(bottom_frame, text="Exit", command=root.quit)
-    exit_button.grid(row=0, column=1, sticky="e", padx=10)
+    exit_button.grid(row=0, column=1, sticky="e", padx=5)
 
     # Run the GUI loop
     root.mainloop()
